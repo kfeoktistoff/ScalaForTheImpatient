4568947076b7f8b87dc568cf264725d02ad8444c
Kirill Feoktistov <fekirill@gmail.com>
2014-06-06 01:51:21 +0400

    Exc 7 for chapter 9 added

---
+++ src/test/scala/Chapter9/Exc7/NonRealNumbersPrinterSpec.scala
@@ -0,0 +1,25 @@
+package Chapter9.Exc7
+
+import org.scalatest.{Matchers, FlatSpec}
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Created by Kirill Feoktistov on 06.06.14
+ */
+
+class NonRealNumbersPrinterSpec extends FlatSpec with Matchers {
+  "A NonRealNumbersPrinter" should "print everything which is not real number" in {
+    val testInput = "-1 0.75 10e2 a123 123a\n10e-2"
+    val testNonRealNumbersPrinter: TestNonRealNumbersPrinter = new TestNonRealNumbersPrinter()
+    testNonRealNumbersPrinter.findAll(testInput)
+    testNonRealNumbersPrinter.output should contain inOrderOnly("-1", "0.75", "10e2", "10e-2")
+  }
+}
+
+class TestNonRealNumbersPrinter extends NonRealNumbersPrinter {
+  val output: ArrayBuffer[String] = new ArrayBuffer[String]
+
+  override def print(line: String) {
+    output += line
+  }
+}
