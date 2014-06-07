a4cfcf2c982580953b166d0d5a3e6141183dcb26
Kirill Feoktistov <fekirill@gmail.com>
2014-06-07 21:23:17 +0400

    Exc 1 for chapter 10 added

---
+++ src/main/scala/Chapter10/Exc1/RectangleLike.scala
@@ -0,0 +1,11 @@
+package Chapter10.Exc1
+
+/**
+ * Created by Kirill Feoktistov on 07.06.14
+ */
+
+trait RectangleLike {
+  def translate(dx: Int, dy: Int) = ???
+
+  def grow(h: Int, v: Int) = ???
+}
