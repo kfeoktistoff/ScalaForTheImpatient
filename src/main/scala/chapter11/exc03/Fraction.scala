7f07a5ac558f18a77f6d1395317d74065ff61695
Kirill Feoktistov <fekirill@gmail.com>
2014-06-18 00:38:30 +0400

    Exercises 1, 3 added for chapter 11

---
+++ src/main/scala/chapter11/exc03/Fraction.scala
@@ -0,0 +1,28 @@
+package chapter11.exc03
+
+/**
+ * Created by Kirill Feoktistov on 17.06.14
+ */
+
+class Fraction(n: Int, d: Int) {
+  private val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d)
+  private val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d)
+
+  override def toString = num + "/" + den
+
+  override def equals(other: Any) = other.isInstanceOf[Fraction] && other.asInstanceOf[Fraction].num == num && other.asInstanceOf[Fraction].den == den
+
+  def sign(a: Int) = if (a > 0) 1 else if (a < 0) -1 else 0
+
+  def gcd(a: Int, b: Int): Int = if (b == 0) abs(a) else gcd(b, a % b)
+
+  def abs(a: Int): Int = math.abs(a)
+
+  def *(other: Fraction) = new Fraction(num * other.num, den * other.den)
+
+  def /(other: Fraction) = new Fraction(num / other.num, den / other.den)
+
+  def +(other: Fraction) = new Fraction(num * other.den + other.num * den, den * other.den)
+
+  def -(other: Fraction) = new Fraction(num * other.den - other.num * den, den * other.den)
+}
