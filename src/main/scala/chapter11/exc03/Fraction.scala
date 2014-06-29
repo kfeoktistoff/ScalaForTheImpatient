ada0030a55080768ced93019385b38e0f5c9dc0d
Kirill Feoktistov <fekirill@gmail.com>
2014-06-29 11:41:06 +0400

    Question text added for exc 1-5 of 11th chapter

--- src/main/scala/chapter11/exc03/Fraction.scala
+++ src/main/scala/chapter11/exc03/Fraction.scala
@@ -1,6 +1,17 @@
 package chapter11.exc03
 
 /**
+ * Implement the Fraction class with operations + - * /. Normalize fractions, for example turning 15/-6 into -5/2.
+ * Divide by the greatest common divisor, like this:
+ * class Fraction(n: Int, d: Int) {
+ * private val num: Int = if (d==0) 1 else n * sign(d) / gcd(n, d);
+ * private val num: Int = if (d==0) 0 else d * sign(d) / gcd(n, d);
+ * override def toString = num + "/" + den
+ * def sign(a: Int) = if (a > 0) 1 else if (a < 0) -1 else 0
+ * def gcd(a: Int, b: Int): Int = if (b==0) abs(a) else gcd(b, a % b)
+ * ...
+ * }
+ *
  * Created by Kirill Feoktistov on 17.06.14
  */
 
