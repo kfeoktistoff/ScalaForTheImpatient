2c1a74ab56160bc07e27a3a1f0a351f172547dc7
Kirill Feoktistov <fekirill@gmail.com>
2014-07-09 23:36:58 +0400

    Encoding mistakes fixed

--- src/main/scala/chapter11/exc03/Fraction.scala
+++ src/main/scala/chapter11/exc03/Fraction.scala
@@ -1,7 +1,8 @@
 package chapter11.exc03
 
 /**
- * Implement the Fraction class with operations + - * /. Normalize fractions, for example turning 15/–6 into –5/2. Divide by the greatest common divisor, like this:
+ * Implement the Fraction class with operations + - * /. Normalize fractions, for example turning 15/-6 into -5/2.
+ * Divide by the greatest common divisor, like this:
  *  class Fraction(n: Int, d: Int) {
  *   private val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d);
  *   private val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d);
